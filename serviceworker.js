69d9ad3edee8e1868824e9d120c2833fb2abb0bd
Pascal Negwer <pn@koempf.de>
2018-04-27 17:05:05 +0200

    new

--- serviceworker.js
+++ serviceworker.js
@@ -1,76 +1,76 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js');
+importScripts('/test/workbox/workbox-v3.1.0/workbox-sw.js');
+//importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js');
 
 let revision = (new Date()).toTimeString();
 let filesToCache = [
   {
-    'url': '/assets/css/main.css',
+    'url': '/test/assets/css/main.css',
     'revision': revision,
   },
   {
-    'url': '/assets/img/logo.svg',
-    'revision': revision,
-  },
-
-  {
-    'url': '/assets/img/bg_0.png',
-    'revision': revision,
-  },
-
-  {
-    'url': '/assets/img/bg_1.png',
+    'url': '/test/workbox/workbox-v3.1.0/workbox-sw.js',
     'revision': revision,
   },
   {
-    'url': '/0.main.js',
-    'revision': revision,
-  },
-  {
-    'url': '/1.main.js',
-    'revision': revision,
-  },
-  {
-    'url': '/2.main.js',
-    'revision': revision,
-  },
-  {
-    'url': '/3.main.js',
-    'revision': revision,
-  },
-  {
-    'url': '/4.main.js',
-    'revision': revision,
-  },
-  {
-    'url': '/5.main.js',
-    'revision': revision,
-  },
-  {
-    'url': '/apiomat.js',
-    'revision': revision,
-  },
-  {
-    'url': '/',
+    'url': '/test/assets/img/logo.svg',
     'revision': revision,
   },
 
   {
-    'url': '/test',
+    'url': '/test/assets/img/bg_0.png',
+    'revision': revision,
+  },
+
+  {
+    'url': '/test/assets/img/bg_1.png',
     'revision': revision,
   },
   {
-    'url': '/main.js',
+    'url': '/test/0.main.js',
     'revision': revision,
   },
   {
-    'url': '/manifest.json',
+    'url': '/test/1.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/2.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/3.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/4.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/5.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/apiomat.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/',
+    'revision': revision,
+  },
+  {
+    'url': '/test/main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/manifest.json',
     'revision': revision,
   },
 ];
 
-workbox.setConfig({
-  debug: true
-});
-workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
+//workbox.setConfig({
+  // debug: true
+//});
+//workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 
 workbox.precaching.precacheAndRoute(filesToCache);
 
