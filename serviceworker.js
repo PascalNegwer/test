40c2e4a34db5220121145e97672b26b57e8776bd
Flip <philipp-reg@web.de>
2018-05-02 17:24:06 +0200

    lasdfu

---
+++ serviceworker.js
@@ -0,0 +1,113 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js');
+
+let revision = (new Date()).toTimeString();
+let filesToCache = [
+  {
+    'url': '/test/assets/css/main.css',
+    'revision': revision,
+  },
+  {
+    'url': '/test/assets/img/bg_blue.png',
+    'revision': revision,
+  },
+  {
+    'url': '/test/assets/img/bg_default.png',
+    'revision': revision,
+  },
+  {
+    'url': '/test/assets/img/bg_mixed.png',
+    'revision': revision,
+  },
+  {
+    'url': '/test/assets/img/bg_orange.png',
+    'revision': revision,
+  },
+  {
+    'url': '/test/assets/img/bg_purple.png',
+    'revision': revision,
+  },
+  {
+    'url': '/test/assets/img/logo.svg',
+    'revision': revision,
+  },
+  {
+    'url': '/test/assets/fonts/wrkFit.svg',
+    'revision': revision,
+  },
+  {
+    'url': '/test/assets/fonts/wrkFit.ttf',
+    'revision': revision,
+  },
+  {
+    'url': '/test/assets/fonts/wrkFit.woff',
+    'revision': revision,
+  },
+  {
+    'url': '/test/0.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/1.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/2.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/3.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/4.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/5.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/6.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/7.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/8.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/9.main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/apiomat.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/',
+    'revision': revision,
+  },
+  {
+    'url': '/test/main.js',
+    'revision': revision,
+  },
+  {
+    'url': '/test/manifest.json',
+    'revision': revision,
+  },
+];
+
+workbox.setConfig({
+   debug: true
+});
+workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
+
+workbox.precaching.precacheAndRoute(filesToCache);
+
+workbox.routing.registerRoute(
+  new RegExp('https://storage.googleapis.com/*'),
+  workbox.strategies.staleWhileRevalidate()
+);
